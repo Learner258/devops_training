e9da0e460ae3542458606e1ee71debd44e01f5b2
Learner258 <101538924+Learner258@users.noreply.github.com>
2022-03-15 12:57:12 +0530

    5

---
+++ task0.cs
@@ -0,0 +1,13 @@
+using System;
+
+class name
+{
+public static void Main(string[] args)
+{
+	string name=args[0];
+	Console.WriteLine("hello "+name+"!!  let us work");
+	Console.WriteLine("******");
+	
+	
+}
+}
